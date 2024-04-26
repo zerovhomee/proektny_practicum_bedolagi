a3fb5f91fcd2e8db8775172ff078994a4814a4d3
Andrei Stekachev <zerov.live@gmail.com>
2024-04-26 20:19:35 +0500

    check github deployment

--- cv2_veniashvilly.py
+++ cv2_veniashvilly.py
@@ -1,7 +1,8 @@
 import cv2
 import numpy as np
 video_path = "C:/Users/mrtar/Downloads/video5206659230827826017 (1).mp4"
-
+check_github = 1000000
+# Это сделал Вениашвиль
 cap = cv2.VideoCapture(video_path)
 cv2.namedWindow('frame')
 kernel = np.ones((5,5), np.uint8)
@@ -57,11 +58,3 @@ while True:
 
 cap.release()
 cv2.destroyAllWindows()
-
-
-
-
-
-
-
-
