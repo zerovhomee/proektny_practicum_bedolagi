3d764b22029655dee2e1792eb5ae531f36c49579
Andrei Stekachev <zerov.live@gmail.com>
2024-05-24 17:14:21 +0500

    raw version of solution

--- decstop.py
+++ decstop.py
@@ -1,19 +1,34 @@
 from tkinter import *
 from cv2_veniashvilly import main
+import sys
 
 
 class Main:
+    def calculate(self):
+        print('Hello world')
+
     def window(self):
-        root = Tk()
-        root.title('My App')
-        lbl1 = Label(root, text='Привет')
-        lbl1.grid(column=0, row=0)
-        lbl1.place(x=10,y=10)
-        btn1 = Button(root, text='Не нажимай', command = main)
-        btn1.place(x= 10 ,y= 50)
+        window = Tk()
+        window.title('Сard recognition')
+        window.geometry('400x300')
+
+        frame = Frame( window, padx = 10, pady = 10 )
+        frame.pack(expand=True)
+
+        lbl_name = Label(frame, text='Card recognition App')
+        lbl_name.grid(row = 1, column = 2)
+
+        btn_start = Button(frame, text='Начать сканирование', command = main)
+        btn_start.grid(row = 5 , column = 2)
+
+        btn_instruction = Button(frame, text='Инструкция по использованию', command = self.calculate)
+        btn_instruction.grid(row = 8 , column = 2)
+
+        btn_quit = Button(frame, text='Выйти', command = sys.exit)
+        btn_quit.grid(row = 10 , column = 2)
 
 
-        root.mainloop()
+        window.mainloop()
 
 
 if __name__ == "__main__":
