a52ede3cf59850f03704db25c5313e3eed831b33
Andrei Stekachev <zerov.live@gmail.com>
2024-05-24 14:38:28 +0500

    Some manipulations

--- decstop.py
+++ decstop.py
@@ -1,6 +1,6 @@
 from tkinter import *
-
-
+from cv2_veniashvilly import main
+''''
 root = Tk()
 root.title('My App') #заголовок
 lbl1 = Label(root, text='Привет')
@@ -8,7 +8,11 @@ lbl1.grid(column=0, row=0)
 lbl1.place(x=10,y=10)
 
 def clicked():
-    print('Я же сказал...')
+    root = Tk()
+    root.title('My App') #заголовок
+    lbl1 = Label(root, text='Я же сказал...')
+    lbl1.grid(column=0, row=0)
+    lbl1.place(x=10,y=10)
 
 btn1 = Button(root, text='Не нажимай', command = clicked)
 btn1.place(x= 10 ,y= 50)
@@ -18,3 +22,15 @@ btn1.place(x= 10 ,y= 50)
 if __name__ == '__main__':
   #выполнение кода до загрузки
   root.mainloop() #отображение окна
+  '''
+
+class Main:
+    def __init__(self):
+        self.cv2_main = self.cv2_veniashvilly.main()
+
+    def main():
+        self.cv2_main()
+
+if __name__ == "__main__":
+    main = Main()
+    main.main()
