fe6ddca3e0b4314c02e3405ba3f84facd85e198e
Andrei Stekachev <zerov.live@gmail.com>
2024-05-24 15:40:36 +0500

    first test

--- decstop.py
+++ decstop.py
@@ -1,36 +1,21 @@
 from tkinter import *
 from cv2_veniashvilly import main
-''''
-root = Tk()
-root.title('My App') #заголовок
-lbl1 = Label(root, text='Привет')
-lbl1.grid(column=0, row=0)
-lbl1.place(x=10,y=10)
 
-def clicked():
-    root = Tk()
-    root.title('My App') #заголовок
-    lbl1 = Label(root, text='Я же сказал...')
-    lbl1.grid(column=0, row=0)
-    lbl1.place(x=10,y=10)
-
-btn1 = Button(root, text='Не нажимай', command = clicked)
-btn1.place(x= 10 ,y= 50)
-
-
-
-if __name__ == '__main__':
-  #выполнение кода до загрузки
-  root.mainloop() #отображение окна
-  '''
 
 class Main:
-    def __init__(self):
-        self.cv2_main = self.cv2_veniashvilly.main()
+    def window(self):
+        root = Tk()
+        root.title('My App')
+        lbl1 = Label(root, text='Привет')
+        lbl1.grid(column=0, row=0)
+        lbl1.place(x=10,y=10)
+        btn1 = Button(root, text='Не нажимай', command = main)
+        btn1.place(x= 10 ,y= 50)
 
-    def main():
-        self.cv2_main()
+
+        root.mainloop()
+
 
 if __name__ == "__main__":
-    main = Main()
-    main.main()
+    program = Main()
+    program.window()
